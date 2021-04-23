c2d1dc49b5e7c45b0771158523bfd02b4b2871f1
Yeahyay <johnandrewbisquera@gmail.com>
2021-04-23 12:35:54 -0400

    Modified EntityArchetypeMethods to handle a different way to store size
    It did not go so well. Will be removed next commit in favor of a more 
    organized system.

--- Feint_Engine/modules/ECS/EntityManagerArchetypeMethods.lua
+++ Feint_Engine/modules/ECS/EntityManagerArchetypeMethods.lua
@@ -7,45 +7,52 @@ function ArchetypeMethods:load(EntityManager)
 	-- ARCHETYPE CONSTRUCTORS
 	function self:newArchetypeFromComponents(components)
 		local archetype = EntityArchetype:new(components)
-		self.archetypes[archetype.archetypeString] = archetype
-		self.archetypeCount = self.archetypeCount + 1
+		self.archetypes[archetype.signature] = archetype
+		self.archetypes.size = self.archetypes.size + 1
+		-- self.archetypeCount = self.archetypeCount + 1
 		-- Feint.Log:logln("Creating archetype " .. archetype.Name)
 
-		self:newArchetypeChunkFromComponents(archetype)
+		self:newArchetypeChunkFromArchetype(archetype)
 		return archetype
 	end
 
 	-- ARCHETYPE GETTERS
-	function self:getArchetypeStringFromComponents(arguments)
-		local stringTable = {}
-		assert(arguments, "no arguments", 3)
-		for i = 1, #arguments do
-			local v = arguments[i]
-			if v.componentData then
-				stringTable[#stringTable + 1] = v.Name
-			end
-		end
-		return table.concat(stringTable)
-	end
-	-- Feint.Util.Memoize(ArchetypeMethods.getArchetypeStringFromComponents)
+	-- function self:getArchetypeSignatureFromComponents(arguments)
+	-- 	local stringTable = {}
+	-- 	assert(arguments, "no arguments", 3)
+	-- 	for i = 1, #arguments do
+	-- 		local v = arguments[i]
+	-- 		if v.componentData then
+	-- 			stringTable[#stringTable + 1] = v.Name
+	-- 		end
+	-- 	end
+	-- 	return table.concat(stringTable)
+	-- end
+	-- Feint.Util.Memoize(ArchetypeMethods.getArchetypeSignatureFromComponents)
 	function self:getArchetypeFromString(string)
 		return self.archetypes[string]
 	end
-	function self:getArchetypeFromComponents(arguments)
-		local archetypeString = self:getArchetypeStringFromComponents(arguments)
-		return self.archetypes[archetypeString]
+	function self:getArchetypeFromComponents(componentArguments)
+		local archetypeSignature = self:getArchetypeSignatureFromComponents(componentArguments)
+		if not self.archetypes[archetypeSignature] then
+			self.archetypes[archetypeSignature] = self:newArchetypeFromComponents(componentArguments)
+		end
+		return self.archetypes[archetypeSignature]
 	end
 
-
 	-- ARCHETYPE CHUNK CONSTRUCTORS
-	function self:newArchetypeChunkFromComponents(archetype)
+	function self:newArchetypeChunkFromArchetype(archetype)
 		local archetypeChunk = EntityArchetypeChunk:new(archetype)
 		-- Feint.Log.log("Creating archetype chunk %s, id: %d\n", archetypeChunk.Name, archetypeChunk.index)
 
+
 		local currentArchetypeChunkTable = self:getArchetypeChunkTableFromArchetype(archetype)
 
-		self.archetypeChunksCount[archetype] = self.archetypeChunksCount[archetype] + 1
-		currentArchetypeChunkTable[self.archetypeChunksCount[archetype]] = archetypeChunk
+		self.archetypeChunks[archetype].size = self.archetypeChunks[archetype].size + 1
+		currentArchetypeChunkTable[self.archetypeChunks[archetype].size] = archetypeChunk
+
+		-- self.archetypeChunksCount[archetype] = self.archetypeChunksCount[archetype] + 1
+		-- currentArchetypeChunkTable[self.archetypeChunksCount[archetype]] = archetypeChunk
 
 		return archetypeChunk
 	end
@@ -54,8 +61,8 @@ function ArchetypeMethods:load(EntityManager)
 	function self:getNextArchetypeChunk(archetype)
 		local currentArchetypeChunkTable = self:getArchetypeChunkTableFromArchetype(archetype)
 		-- print(archetype)
-		assert(self.archetypes[archetype.archetypeString],
-			string.format("Archetype %s does not exist", archetype.archetypeString), 2)
+		assert(self.archetypes[archetype.signature],
+			string.format("Archetype %s does not exist", archetype.signature), 2)
 		local currentArchetypeChunkTableCount = self.archetypeChunksCount[archetype]
 
 		local currentArchetypeChunk = currentArchetypeChunkTable[currentArchetypeChunkTableCount]
@@ -63,7 +70,7 @@ function ArchetypeMethods:load(EntityManager)
 		if currentArchetypeChunk:isFull() then
 			-- Feint.Log:logln(currentArchetypeChunk.numEntities * currentArchetypeChunk.entitySizeBytes)
 			-- Feint.Log:logln((currentArchetypeChunk.numEntities * currentArchetypeChunk.entitySizeBytes) / 1024)
-			currentArchetypeChunk = self:newArchetypeChunkFromComponents(archetype)
+			currentArchetypeChunk = self:newArchetypeChunkFromArchetype(archetype)
 		end
 		return currentArchetypeChunk
 	end
@@ -71,9 +78,18 @@ function ArchetypeMethods:load(EntityManager)
 	function self:getArchetypeChunkTableFromArchetype(archetype)
 		local currentArchetypeChunkTable = self.archetypeChunks[archetype]
 		if not currentArchetypeChunkTable then
-			self.archetypeChunks[archetype] = {}
-			self.archetypeChunksCount[archetype] = 0
-			currentArchetypeChunkTable = self.archetypeChunks[archetype]
+			self.archetypeChunks[archetype] = {size = 0}
+			-- self.archetypeChunks[archetype] = setmetatable({}, {__index = {size = 0},
+				-- __newindex = function(self, k, v)
+				-- 	if type(k) ~= "number" then
+				-- 		error("not a number", 2)
+				-- 	end
+				-- end
+				-- })
+
+			-- self.archetypeChunksCount[archetype] = 0
+			-- currentArchetypeChunkTable = self.archetypeChunks[archetype]
+			currentArchetypeChunkTable  = self:newArchetypeChunkFromArchetype(archetype)
 		end
 		return currentArchetypeChunkTable
 	end
