3212e692d68fdfad380edc3262b10fe1b27c7b38
John Andrew <johnandrewbisquera@gmail.com>
2021-11-09 21:27:15 -0500

    Added Cute for unit tests

---
+++ Cute-0_4_0/main.lua
@@ -0,0 +1,22 @@
+local cute = require("cute")
+local shapes = require("src.shapes")
+
+love.load = function (args)
+  cute.go(args)
+end
+
+local _dt = 0
+
+love.update = function (dt)
+  _dt = _dt + dt
+end
+
+love.draw = function (dt)
+  shapes.tiles()
+  shapes.circle(_dt % 100)
+  cute.draw(love.graphics)
+end
+
+love.keypressed = function (key)
+  cute.keypressed(key)
+end
